dda33cc9dc458a8676619bc439d257f3449b32f4
Harsh <harshverma3206@gmail.com>
2026-05-06 07:33:08 +0530

    Final Board

--- src/Main/HeroPage.jsx
+++ src/Main/HeroPage.jsx
@@ -6,7 +6,7 @@ const HeroPage = () => {
 
     const [row, setRow] = useState([])
     const [column, setColumn] = useState([])
-    const [blockElements, setBlockElements] = useState([])
+    // const [blockElements, setBlockElements] = useState([])
 
     // Calculate the number of rows and columns based on the game board size
     useEffect(() => {
@@ -17,8 +17,12 @@ const HeroPage = () => {
             const blockHeight = 20;
             const blockWidth = 20;
 
-            const columns = Math.floor(element.clientWidth / blockWidth);
             const rows = Math.floor(element.clientHeight / blockHeight);
+            const columns = Math.floor(element.clientWidth / blockHeight);
+
+
+            console.log(rows);
+            console.log(columns);
 
             // setFinalBox(columns * rows);
             setRow(rows);
@@ -30,49 +34,56 @@ const HeroPage = () => {
     }, [])
 
     // Array for all block elements
-    useEffect(() => {
-        const allBoxElements = document.querySelectorAll('.box');
-        setBlockElements(Array.from(allBoxElements));
-        window.allBlockElements = allBoxElements;
-    }, [row, column]);
+    // useEffect(() => {
+    //     const allBoxElements = document.querySelectorAll('.box');
+    //     setBlockElements(Array.from(allBoxElements));
+    //     window.allBlockElements = allBoxElements;
+    // }, [row, column]);
 
     //create a snake array
-    const snake = [
-        { x: 10, y: 1 },
-        { x: 0, y: 0 },
-        { x: 0, y: 0 }
-    ]
+    // const snake = [
+    //     { x: 10, y: 1 },
+    //     { x: 0, y: 0 },
+    //     { x: 0, y: 0 }
+    // ]
 
-    useEffect(() => {
-        const renderSnake = () => {
-            snake.forEach((segment, index) => {
-                allBlockElements[segment.x + segment.y].classList.add('snake');
-            });
-        };
+    // useEffect(() => {
+    //     const renderSnake = () => {
+    //         snake.forEach((segment, index) => {
+    //             allBlockElements[segment.x + segment.y].classList.add('snake');
+    //         });
+    //     };
 
-        if (blockElements.length > 0) {
-            renderSnake();
-        }
-    }, [blockElements, column]);
+    //     if (blockElements.length > 0) {
+    //         renderSnake();
+    //     }
+    // }, [blockElements, column]);
 
     return (
         <div>
-            <div className='py-10! px-40! flex gap-10! h-screen'>
-                <div className='w-fit flex flex-col gap-3 px-8!'>
-                    <div className='border border-gray-300 py-2! px-4! rounded-2xl'><h1>High Score:<span> 0 </span></h1></div>
-                    <div className='border border-gray-300 py-2! px-4! rounded-2xl'><h1>Score:<span> 0 </span></h1></div>
-                    <div className='border border-gray-300 py-2! px-4! rounded-2xl'><h1>Time:<span> 00-00 </span></h1></div>
+            <div className='py-10! px-2! lg:px-40! flex flex-col gap-10! h-screen'>
+                <div className='flex flex-col items-center'>
+                    <h2 className='text-4xl! text-center mb-8! lg:mb-15! font-bold!'>Snaky</h2>
+                    <div className='flex flex-col lg:flex-row gap-3 px-8! items-center'>
+                        <div className='border border-gray-300 py-2! px-5! rounded-2xl min-w-80 '><h1>High Score : <span> 0 </span></h1></div>
+                        <div className='border border-gray-300 py-2! px-5! rounded-2xl min-w-80 '><h1>Score : <span> 0 </span></h1></div>
+                        <div className='border border-gray-300 py-2! px-5! rounded-2xl min-w-80 '><h1>Time : <span> 00-00 </span></h1></div>
+                    </div>
                 </div>
-                <div ref={gameBoardRef} className='flex-1 grid grid-cols-[repeat(auto-fill,minmax(20px,1fr))] grid-rows-[repeat(auto-fill,minmax(20px,1fr))]'>
-                    {Array.from({ length: row }).map((_, rowIndex) => (
-                        Array.from({ length: column }).map((_, colIndex) => (
-                            <div key={`${rowIndex}-${colIndex}`} className='box text-[1rem]'>
-                            </div>
-                        ))
+                <div ref={gameBoardRef} className='bg-amber-600/15 flex-1 grid grid-rows-[repeat(auto-fill,minmax(20px,1fr))]'>
+                    {Array.from({ length: row }).map((_, i) => (
+                        <div key={i} className='grid grid-cols-[repeat(auto-fill,minmax(20px,1fr))]'>
+                            {Array.from({ length: column }).map((_, j) => (
+                                <div key={j} style={{ height: "20px", width: "20px" }} className="box">
+                                    {`${i}-${j}`}
+                                </div>
+                            ))}
+                        </div>
                     ))}
+
                 </div>
             </div>
-            <div className='h-screen bg-pink-500'>Hye</div>
+            {/* <div className='h-screen bg-pink-500'>Hye</div> */}
         </div>
     )
 }
