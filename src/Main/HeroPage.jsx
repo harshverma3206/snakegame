aa2e7d53c0b0d492cd1944c06505cabd4a50ad5a
Harsh <harshverma3206@gmail.com>
2026-05-08 11:47:04 +0530

    Snake Movement Completed

--- src/Main/HeroPage.jsx
+++ src/Main/HeroPage.jsx
@@ -44,10 +44,10 @@ const HeroPage = () => {
 
     //Snake body coordinates
     const snake = [
-        { x: 0, y: 3 },
-        { x: 0, y: 2 },
-        { x: 0, y: 1 },
-        { x: 0, y: 0 }
+        { x: 0, y: 13 },
+        { x: 0, y: 12 },
+        { x: 0, y: 11 },
+
     ]
 
     // Function to render the snake on the game board
@@ -58,14 +58,18 @@ const HeroPage = () => {
     }
 
     // Render the snake on the game board whenever the block elements are updated
+    let direction = 'left';
     setInterval(() => {
-
         let snakeHead = null;
-        let direction = 'right';
 
         if (direction === 'right') {
-            snakeHead = { x: snake[0].x + 1, y: snake[0].y }
             snakeHead = { x: snake[0].x, y: snake[0].y + 1 }
+        } else if (direction === 'left') {
+            snakeHead = { x: snake[0].x, y: snake[0].y - 1 }
+        } else if (direction === 'down') {
+            snakeHead = { x: snake[0].x + 1, y: snake[0].y }
+        } else if (direction === 'up') {
+            snakeHead = { x: snake[0].x - 1, y: snake[0].y }
         }
 
         snake.forEach((segment) => {
@@ -76,7 +80,21 @@ const HeroPage = () => {
         snake.pop();
 
         renderSnake()
-    }, 300)
+    }, 50)
+
+    //control the snake movement using arrow keys
+    addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowRight' && direction !== 'left') {
+            direction = 'right';
+        } else if (e.key === 'ArrowLeft' && direction !== 'right') {
+            direction = 'left';
+        } else if (e.key === 'ArrowDown' && direction !== 'up') {
+            direction = 'down';
+        } else if (e.key === 'ArrowUp' && direction !== 'down') {
+            direction = 'up';
+        }
+    })
+
 
     // useEffect(() => {
     //     // if (blockElements.length === 0) return
