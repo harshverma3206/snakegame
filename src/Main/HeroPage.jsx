665e678ca59489b75a1861dd461cd49ca7f37ae4
Harsh <harshverma3206@gmail.com>
2026-05-08 12:38:06 +0530

    Snake Food Consumming

--- src/Main/HeroPage.jsx
+++ src/Main/HeroPage.jsx
@@ -2,13 +2,26 @@ import React, { use, useEffect, useRef, useState } from 'react'
 
 const HeroPage = () => {
 
+    //useRef Conditions
     const gameBoardRef = useRef()
     const isFirstRender = useRef(true);
 
+    //useState Conditions
     const [row, setRow] = useState([])
     const [column, setColumn] = useState([])
     const [blockElements, setBlockElements] = useState([])
 
+    //Global Variables
+    let direction = 'left';
+    let intervalId = null;
+
+    //Generate food
+    let food = {
+        x: Math.floor(Math.random() * row),
+        y: Math.floor(Math.random() * column)
+    }
+
+    //useEffect Conditions
     useEffect(() => {
         // Calculate the number of rows and columns based on the game board size
         const element = gameBoardRef.current;
@@ -21,10 +34,10 @@ const HeroPage = () => {
             const rows = Math.floor(element.clientHeight / blockHeight);
             const columns = Math.floor(element.clientWidth / blockHeight);
 
-            // setFinalBox(columns * rows);
             setRow(rows);
             setColumn(columns);
         };
+
         calculateGrid();
 
         // Array of board blocks
@@ -47,21 +60,18 @@ const HeroPage = () => {
         { x: 0, y: 13 },
         { x: 0, y: 12 },
         { x: 0, y: 11 },
-
     ]
 
-    // Function to render the snake on the game board
-    const renderSnake = () => {
-        snake.forEach((segment) => {
-            blockElements[`${segment.x}-${segment.y}`].classList.add('snakeColor')
-        })
-    }
+    // Render Snake to gameboard
+    const render = () => {
 
-    // Render the snake on the game board whenever the block elements are updated
-    let direction = 'left';
-    setInterval(() => {
+        //Food rendering
+        blockElements[`${food.x}-${food.y}`].classList.add('foodColor')
+
+        //calculate snake head
         let snakeHead = null;
 
+        //Calculate the new head position based on the current direction
         if (direction === 'right') {
             snakeHead = { x: snake[0].x, y: snake[0].y + 1 }
         } else if (direction === 'left') {
@@ -72,6 +82,22 @@ const HeroPage = () => {
             snakeHead = { x: snake[0].x - 1, y: snake[0].y }
         }
 
+        //Control game over conditions
+        if (snakeHead.x < 0 || snakeHead.x >= row || snakeHead.y < 0 || snakeHead.y >= column) {
+            alert('Game Over');
+            clearInterval(intervalId)
+        }
+
+        //food eating conditions
+        if (snakeHead.x === food.x && snakeHead.y === food.y) {
+            blockElements[`${food.x}-${food.y}`].classList.remove('foodColor')
+            food = {
+                x: Math.floor(Math.random() * row),
+                y: Math.floor(Math.random() * column)
+            }
+        }
+
+        //Remove snake color from the tail segment
         snake.forEach((segment) => {
             blockElements[`${segment.x}-${segment.y}`].classList.remove('snakeColor')
         })
@@ -79,8 +105,17 @@ const HeroPage = () => {
         snake.unshift(snakeHead);
         snake.pop();
 
-        renderSnake()
-    }, 50)
+        //fill snake color
+        snake.forEach((segment) => {
+            blockElements[`${segment.x}-${segment.y}`].classList.add('snakeColor')
+        })
+
+    }
+
+    // Render the snake on the game board whenever the block elements are updated
+    intervalId = setInterval(() => {
+        render()
+    }, 200)
 
     //control the snake movement using arrow keys
     addEventListener('keydown', (e) => {
@@ -94,16 +129,6 @@ const HeroPage = () => {
             direction = 'up';
         }
     })
-
-
-    // useEffect(() => {
-    //     // if (blockElements.length === 0) return
-    //     if (isFirstRender.current) {
-    //         isFirstRender.current = false;
-    //         return;
-    //     }
-
-    // }, [blockElements])
 
     return (
         <div>
