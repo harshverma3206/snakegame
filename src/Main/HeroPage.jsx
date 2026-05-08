71e3364986be2f5b2ac9a1e2c40d1c4320e9e706
Harsh <harshverma3206@gmail.com>
2026-05-08 06:27:23 +0530

    Snake Understanding

--- src/Main/HeroPage.jsx
+++ src/Main/HeroPage.jsx
@@ -3,19 +3,20 @@ import React, { use, useEffect, useRef, useState } from 'react'
 const HeroPage = () => {
 
     const gameBoardRef = useRef()
+    const isFirstRender = useRef(true);
 
     const [row, setRow] = useState([])
     const [column, setColumn] = useState([])
     const [blockElements, setBlockElements] = useState([])
 
-    // Calculate the number of rows and columns based on the game board size
     useEffect(() => {
+        // Calculate the number of rows and columns based on the game board size
         const element = gameBoardRef.current;
         if (!element) return;
 
         const calculateGrid = () => {
-            const blockHeight = 20;
-            const blockWidth = 20;
+            const blockHeight = 10
+            const blockWidth = 10
 
             const rows = Math.floor(element.clientHeight / blockHeight);
             const columns = Math.floor(element.clientWidth / blockHeight);
@@ -24,13 +25,9 @@ const HeroPage = () => {
             setRow(rows);
             setColumn(columns);
         };
-
         calculateGrid();
 
-    }, [])
-
-    // Array of board blocks
-    useEffect(() => {
+        // Array of board blocks
         const allBoxElements = document.querySelectorAll('.box');
         if (allBoxElements.length === 0) return;
 
@@ -40,22 +37,35 @@ const HeroPage = () => {
             const column = e.dataset.column;
             block[`${row}-${column}`] = e;
         })
-
         setBlockElements(block);
+        console.log(block);
 
-    }, [row, column]);
+    }, [row, column])
 
-    //create a snake array
+    //Snake body coordinates
     const snake = [
-        {
-            x: 0,
-            y: 0,
-        },
-        {
-            x: 0,
-            y: 1,
-        }
+        { row: 5, column: 4 },
+        { row: 6, column: 4 },
+        { row: 7, column: 4 },
+        { row: 8, column: 4 },
     ]
+
+    // Function to render the snake on the game board
+    const renderSnake = () => {
+        snake.forEach((segment) => {
+            blockElements[`${segment.row}-${segment.column}`].classList.add('snake');
+        })
+    }
+
+    // Render the snake on the game board whenever the block elements are updated
+    useEffect(() => {
+        // if (blockElements.length === 0) return
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+        renderSnake()
+    }, [blockElements])
 
     return (
         <div>
@@ -68,15 +78,15 @@ const HeroPage = () => {
                         <div className='border border-gray-300 py-2! px-5! rounded-2xl min-w-80 '><h1>Time : <span> 00-00 </span></h1></div>
                     </div>
                 </div>
-                <div ref={gameBoardRef} className='flex-1 grid grid-rows-[repeat(auto-fill,minmax(20px,1fr))] mx-5!'>
+                <div ref={gameBoardRef} className='flex-1 grid grid-rows-[repeat(auto-fill,minmax(10px,1fr))] mx-5! lg:mx-53! bg-amber-800'>
                     {Array.from({ length: row }).map((_, i) => (
-                        <div key={i} className='grid grid-cols-[repeat(auto-fill,minmax(20px,1fr))]'>
+                        <div key={i} className='grid grid-cols-[repeat(auto-fill,minmax(10px,1fr))]'>
                             {Array.from({ length: column }).map((_, j) => (
                                 <div
                                     data-row={i}
                                     data-column={j}
                                     key={j}
-                                    style={{ height: "20px", width: "20px" }}
+                                    style={{ height: "10px", width: "10px" }}
                                     className="box">
                                     {/* {`${i}-${j}`} */}
                                 </div>
